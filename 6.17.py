65753b984f548ff958ceda8861f78afa59b88838
annabellevpoer <85715378+annabellevpoer@users.noreply.github.com>
2021-10-13 21:57:32 -0500

    Add files via upload

---
+++ 6.17.py
@@ -0,0 +1,13 @@
+# Annabelle Poer PSID: 1798854
+
+word = input()
+
+word = word.replace('i','!')
+word = word.replace('a','@')
+word = word.replace('m','M')
+word = word.replace('B','8')
+word = word.replace('o','.')
+
+word = word + 'q*s'
+
+print(word)
